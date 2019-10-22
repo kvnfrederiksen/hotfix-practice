55d6bdbf0ed6aeb97adf1cf9abb13dac9aeba942
Adrian Ossi <aossi@bullhorn.com>
2019-10-22 09:19:51 -0500

    Add another function

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const fakeFunction = () => {
     console.log('this is a fake function');
 };
+
+const anotherFakeFunction = () => {
+    console.log('this is the second fake function');
+};
+
 fakeFunction();
+anotherFakeFunction();
